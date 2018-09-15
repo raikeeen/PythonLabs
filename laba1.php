0f11ce052a24f6ff3d00083b2d88afe5a2234dbe
raikeeen <sonnikcs@mail.ru>
2018-09-15 15:50:04 +0500

    Структура элемента

--- laba1.php
+++ laba1.php
@@ -1,12 +1,26 @@
-<?php
+<?
 /**
  * Created by PhpStorm.
  * User: raiken
  * Date: 15.09.18
  * Time: 13:25
- */
-echo 'Введите температуру';
-$temp = fgets(STDIN, 255);
+ */?>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Конвентер</title>
+</head>
+<body>
+<h1 align="center">Конвентер температур</h1>
+<p>Введите температуру</p>
+<input type="text" size="40">
+<? ?>
+
+</body>
+</html>
+
+
+$temp = fgets(STDIN, 255); //$number
 echo 'Введите во что перевести в фаренгейты F или цельсия C';
 $a=fgets(STDIN, 255);
 if($a==='F')
